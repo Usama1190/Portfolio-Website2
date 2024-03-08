e960b7c4a82149dffd9925398f559fc96c699309
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-03-08 06:30:31 -0800

    Portfolio Website

---
+++ script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+    strings:["", "Web Designer", "web Developer", "Graphic Designer", "Software Engineer", "Programmer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
